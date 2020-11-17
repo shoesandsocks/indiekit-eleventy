999056d4bf82dc1428be2d2bca7890818c277d1b
RL Brown <rich@pineandvine.com>
2020-11-17 13:47:26 -0500

    basic article styling and frontpage display

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const CleanCSS = require("clean-css");
+const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   // minify css (see Hot Tip on eleventy docs)
@@ -6,6 +7,17 @@ module.exports = function (eleventyConfig) {
     return new CleanCSS({}).minify(code).styles;
   });
 
+  eleventyConfig.addFilter("simpleDate", function (date) {
+    return DateTime.fromJSDate(new Date(date), {
+      zone: "America/New_York",
+    })
+      .toLocaleString(DateTime.DATETIME_MED)
+      .replace(/, \d{4}/, "");
+  });
+  eleventyConfig.addFilter("debug", function (obj) {
+    console.log(obj);
+    // return JSON.stringify(obj, null, 4);
+  });
   eleventyConfig.addPassthroughCopy("favicon.ico");
   return {
     passthroughFileCopy: true,
